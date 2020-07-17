5b89b734baa522d81497d6dea1a7328c8444c8d9
Sven <app@svensfinlay.tech>
2020-07-17 18:17:36 +0100

    Working on data, moving to prio class

--- Sight_++_Solution/Intel_Sight_PP/src/classification_result.hpp
+++ Sight_++_Solution/Intel_Sight_PP/src/classification_result.hpp
@@ -1,8 +1,27 @@
-﻿#pragma once
+﻿//#pragma once
+#include <iostream>
+#include <string>
+
+
+struct point {
+	int x;
+	int y;
+};
+
+struct ClassificationItem
+{
+	char name[100];
+	int distance;
+	point topLeft;
+	point topRight;
+	point bottomRight;
+};
 
 struct ClassificationResult {
-
 	// TODO What results to store?
+	
+	std::string model_name;
+	std::vector<ClassificationItem> objects;
 	
 };
 
