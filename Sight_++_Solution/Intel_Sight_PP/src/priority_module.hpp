2bb64267d43148e831599adcd27d22ac00beba4c
Sven <app@svensfinlay.tech>
2020-07-18 23:52:49 +0100

    Quicksort prioritiser connected, working on sorting constructor

--- Sight_++_Solution/Intel_Sight_PP/src/priority_module.hpp
+++ Sight_++_Solution/Intel_Sight_PP/src/priority_module.hpp
@@ -1,17 +1,23 @@
 #pragma once
-
 #include <vector>
 #include <iostream>
 #include "classification_result.hpp"
 
-class priority_module{
+class priority_module {
 protected:
 	std::string name;
+	std::vector<ClassificationItem> all_data ;
 public:
 	virtual std::string get_name() { return name; }
+	virtual void sort_vector(std::string name, std::vector<PrioritisedClassificationResult>& result);
 	virtual int compare(ClassificationItem& item_1, ClassificationItem& item_2) = 0;
-	virtual void sort_vector(std::vector<ClassificationResult>& data, std::vector<PrioritisedClassificationResult>& result) = 0;
-	virtual std::vector<PrioritisedClassificationResult> run(std::vector<ClassificationResult>& results) = 0;
+	virtual std::vector<PrioritisedClassificationResult> run() = 0;
+	
+	void build_data(std::vector<ClassificationResult>& data);
+	int partition(std::vector<ClassificationItem>& values, int left, int right);
+	void quicksort(std::vector<ClassificationItem>& data, int left, int right);
+
+	
 
 };
 
