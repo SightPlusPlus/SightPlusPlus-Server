914519e2682f3e6ca0bdf4d7d2953a105935527f
Gísli <GisliGeorgs@users.noreply.github.com>
2020-07-23 21:48:38 +0000

    Drawing rectangles on output window stream

--- Sight_++_Solution/Intel_Sight_PP/src/caffe_impl.cpp
+++ Sight_++_Solution/Intel_Sight_PP/src/caffe_impl.cpp
@@ -15,7 +15,6 @@ struct CaffeModelImpl : public ModelInterface {
 
 	cv::dnn::Net net;
 	std::vector<std::string> class_names;
-	cv::Rect crop;
 	
 	const size_t inWidth = 640;
 	const size_t inHeight = 480;
@@ -25,14 +24,13 @@ struct CaffeModelImpl : public ModelInterface {
 
 	const float confidence_threshold = 0.8f;
 	
-	CaffeModelImpl(std::string prototxt_path, std::string caffemodel_path, const std::string class_names_path, const rs2::video_stream_profile& profile)
+	CaffeModelImpl(std::string prototxt_path, std::string caffemodel_path, const std::string class_names_path)
 	{
 		std::cout << "Constructing a caffe model impl\n";
 		
 		net = cv::dnn::readNetFromCaffe(prototxt_path, caffemodel_path);
 		class_names = read_class_name_file(class_names_path);
 
-		crop = create_rect(profile);
 	}
 
 	// This is the same as the rs-dnn example
@@ -49,10 +47,9 @@ struct CaffeModelImpl : public ModelInterface {
 
 		cv::Mat detection_matrix(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
 
-		color_matrix = color_matrix(crop);
-		depth_matrix = depth_matrix(crop);
+		ClassificationResult classification_result("caffe");
 
-		ClassificationResult classification_result("caffe-selftrained");
+		std::cout << "Classified, foreaching" << std::endl;
 		
 		for(auto i = 0; i < detection_matrix.rows; i++)
 		{
@@ -77,26 +74,10 @@ struct CaffeModelImpl : public ModelInterface {
 				point left_bottom(x_left_bottom, y_left_bottom);
 				point right_top(x_right_top, y_right_top);
 				
-				classification_result.objects.emplace_back(class_names[object_class], distance, left_bottom,right_top);
+				classification_result.objects.emplace_back(class_names[object_class], distance, left_bottom, right_top);
 			}
 		}
 		
 		return classification_result;
 	}
-
-	cv::Rect create_rect(const rs2::video_stream_profile& profile) const
-	{
-		cv::Size crop_size;
-		if (profile.width() / static_cast<float>(profile.height()) > WHRatio)
-		{
-			crop_size = cv::Size(static_cast<int>(profile.height() * WHRatio), profile.height());
-		}
-		else
-		{
-			crop_size = cv::Size(profile.width(), static_cast<int>(profile.width() / WHRatio));
-		}
-
-		return cv::Rect(cv::Point((profile.width() - crop_size.width) / 2, (profile.height() - crop_size.height) / 2), crop_size);
-	}
-
 };
