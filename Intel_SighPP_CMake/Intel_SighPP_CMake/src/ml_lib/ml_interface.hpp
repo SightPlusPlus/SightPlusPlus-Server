c557c2cf65b68e1dbd01edcfaa6d2fa339bd6f10
xingdcen <ucabxce@ucl.ac.uk>
2020-08-25 18:40:06 +0100

    ml_interface re-added and CMakeLists modified

---
+++ Intel_SighPP_CMake/Intel_SighPP_CMake/src/ml_lib/ml_interface.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <opencv2/dnn.hpp>
+#include "librealsense2/rs.hpp"
+#include "../classification_result.hpp"
+
+struct ModelInterface {
+	/// <summary>
+ 	/// Function to identify objects and return the resutls 
+ 	/// (obejct class names, relative location, distance and confidence)
+ 	/// </summary>
+ 	/// <param name="color_matrix">color matrix obtained from the camera</param>
+ 	/// <param name="depth_matrix">depth matrix obtained from the camera</param>
+	virtual ClassificationResult do_work(cv::Mat color_matrix, cv::Mat depth_matrix) = 0;
+
+};
