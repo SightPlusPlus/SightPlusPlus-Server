533611a295a1cd88e5f0bcdd8fc76ba74dd5238c
Sven <app@svensfinlay.tech>
2020-09-15 03:34:04 +0100

    BAsic testing for priortiser redone!

--- Intel_SighPP_CMake/Intel_SighPP_CMake/src/ml_lib/ml_interface.hpp
+++ Intel_SighPP_CMake/Intel_SighPP_CMake/src/ml_lib/ml_interface.hpp
@@ -14,8 +14,10 @@ struct ModelInterface {
 	float WHRatio = inWidth / (float)inHeight;
 	float inScaleFactor = 0.007843f;
 	float meanVal = 127.5;
-	float confidence_threshold = 0.8f;
+	float confidence_threshold = 0.7f;
+
   virtual ~ModelInterface() = default;
+
 	/// <summary>
  	/// Function to identify objects and return the resutls 
  	/// (obejct class names, relative location, distance and confidence)
@@ -23,6 +25,7 @@ struct ModelInterface {
  	/// <param name="color_matrix">color matrix obtained from the camera</param>
  	/// <param name="depth_matrix">depth matrix obtained from the camera</param>
 	virtual ClassificationResult do_work(cv::Mat color_matrix, cv::Mat depth_matrix) = 0;
+
 	/// <summary>
  	/// Function to set frame's resolution in object detection networks
  	/// </summary>
